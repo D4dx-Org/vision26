33482ee3639cafcec7b2f3ce6e25f77e76f63f20
Muhammed Ameen <ameens.in19@gmail.com>
2024-11-14 12:52:37 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    bool isTestMode = Platform.isLinux;
+    // bool isTestMode = Platform.isLinux;
 
     return MultiProvider(
       providers: [
